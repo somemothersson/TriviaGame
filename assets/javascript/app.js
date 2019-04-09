e2832359ea2d98a9f28cca507cc715a92984bbc1
Stephen Malito <stevemalito@gmail.com>
2019-04-08 23:32:34 -0500

    modify psuedo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,31 +75,43 @@ let trivia = {
     },
 };
 let choices = $("<choices>")
-let btn1 = $("<button>")
+let btn1 = $("<button>").text(trivia.qOne.answers[i].text)
 let btn2 = $("<button>")
 let btn3 = $("<button>")
 let btn4 = $("<button>")
 let question = "";
 let result = "";
-let gif = ""
+let gif = "";
 let wins = 0;
 let loses = 0;
 console.log(trivia.qOne.question, trivia.qOne.answers[2].text)
-//click start button to begin the game - only to begin
-//timer will be displayed
-    //timer will start counting down from 25
-//the question will be displayed 
-//4 answer buttons will be displayed
-//If the correct answer is clicked 
-    //display if the choice was correct or incorrect for 5 seconds
-    //timer will stop
-    //1 point will be added to total hidden score, displayed only at the end
-    //related gif will display to the question/answer
-    //go to the next question automatically after 3-5 seconds
-//else the incorrect answer is chosen or no answer is chosen
-     //timer will stop
-    //1 point will be subtracted from total hidden score, displayed only at the end
-    //display of the correct for 5 seconds
+
+//function click to start game
+    //click start button to begin the game - only to begin
+
+
+
+//function question load
+        //timer will be displayed
+        //timer will start counting down from 25
+    //the question will be displayed 
+    //4 answer buttons will be displayed
+    //If the correct answer is clicked 
+        //display if the choice was correct or incorrect for 5 seconds
+        //timer will stop
+        //1 point will be added to total hidden score, displayed only at the end
         //related gif will display to the question/answer
-    //go to the next question automatically after 3-5 seconds
+        //go to the next question automatically after 3-5 seconds
+    //else the incorrect answer is chosen or no answer is chosen
+        //timer will stop
+        //1 point will be subtracted from total hidden score, displayed only at the end
+        //display of the correct for 5 seconds
+            //related gif will display to the question/answer
+        //go to the next question automatically after 3-5 seconds
+//function question load(one)
+//function question load(two)
+//function question load(three)
+//function question load(four)
+//function question complete
+
 //after 5 questions the game will end and show score.
