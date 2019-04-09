4180694bab5972a824dfe9485f9ce4775dfd6ed0
Stephen Malito <stevemalito@gmail.com>
2019-04-08 22:33:16 -0500

    psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//click start button to begin the game - only to begin
+    //when started the timer will start counting down from 25
+//the question will be displayed 
+//4 answer buttons will be displayed
+//If the correct answer is clicked 
+    //timer will stop
+    //1 point will be added to total hidden score, displayed only at the end
+    //display if the choice was correct or incorrect for 5 seconds
+        //related gif will display to the question/answer
+    //go to the next question automatically after 3-5 seconds
+//else the incorrect answer is chosen or no answer is chosen
+     //timer will stop
+    //1 point will be subtracted from total hidden score, displayed only at the end
+    //display of the correct for 5 seconds
+        //related gif will display to the question/answer
+    //go to the next question automatically after 3-5 seconds
+//after 5 questions the game will end and show score.
